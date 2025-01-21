dc01116b5791108a5080da6e924733d4f8d1db70
Abdullah Bin Anis <rugusakii@gmail.com>
2025-01-21 23:00:31 +0500

    UI Fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,12 @@ import NexbiLogo from "../assets/nexbi_logo.svg";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [currentHash, setCurrentHash] = useState("");
 
   useEffect(() => {
+    // Handle scroll logic
     const handleScroll = () => {
-      // If the scroll position is greater than 0, we consider it scrolled
-      if (window.scrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+      setIsScrolled(window.scrollY > 0);
     };
 
     // Attach the scroll event listener
@@ -23,22 +20,67 @@ const Header = () => {
     };
   }, []);
 
+  useEffect(() => {
+    // Handle hash change logic
+    const handleHashChange = () => {
+      setCurrentHash(window.location.hash);
+    };
+
+    // Initialize the hash on component mount
+    setCurrentHash(window.location.hash);
+
+    // Attach hash change event listener
+    window.addEventListener("hashchange", handleHashChange);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
   return (
     <header
       className="fixed !z-50 top-6 left-1/2 transform -translate-x-1/2 max-w-4xl w-full flex justify-between items-center px-4 py-2.5 text-white rounded-full border border-[#FFFFFF08] transition-all duration-300 ease-in-out"
       style={{
         background: isScrolled
-          ? "linear-gradient(90.29deg, rgba(9, 10, 12, 0.7) 41.9%, rgba(7, 7, 9, 0.7) 65%)" // Hex background when scrolled
-          : "linear-gradient(90.29deg, rgba(9, 10, 12, 0.02) 11.9%, rgba(7, 7, 9, 0.02) 76.85%)", // Linear gradient when at the top
+          ? "linear-gradient(90.29deg, rgba(9, 10, 12, 0.7) 41.9%, rgba(7, 7, 9, 0.7) 65%)"
+          : "linear-gradient(90.29deg, rgba(9, 10, 12, 0.02) 11.9%, rgba(7, 7, 9, 0.02) 76.85%)",
       }}
     >
-      <img src={NexbiLogo} width="100px" height="18px" alt="nexbi_logo" className="object-contain" />
+      <img
+        src={NexbiLogo}
+        width="100px"
+        height="18px"
+        alt="nexbi_logo"
+        className="object-contain"
+      />
       <nav className="space-x-12 items-center hidden md:flex">
-        <a href="#" className="hover:text-gray-400">Home</a>
-        <a href="#whyus" className="text-[#F7F8F8] opacity-[0.45]">Why Us</a>
-        <a href="#how_it_works" className="text-[#F7F8F8] opacity-[0.45]">How it works</a>
+        <a
+          style={{ opacity: currentHash === "" ? 1 : 0.45 }}
+          href="#"
+          className="hover:text-gray-400"
+        >
+          Home
+        </a>
+        <a
+          style={{ opacity: currentHash === "#whyus" ? 1 : 0.45 }}
+          href="#whyus"
+          className="text-[#F7F8F8]"
+        >
+          Why Us
+        </a>
+        <a
+          style={{ opacity: currentHash === "#how_it_works" ? 1 : 0.45 }}
+          href="#how_it_works"
+          className="text-[#F7F8F8]"
+        >
+          How it works
+        </a>
       </nav>
-      <a href="#waitlist" className="bg-[#FFFFFF] opacity-100 text-[#2F3031] text-md px-5 py-2.5 rounded-full">
+      <a
+        href="#waitlist"
+        className="bg-[#FFFFFF] opacity-100 text-[#2F3031] text-md px-5 py-2.5 rounded-full"
+      >
         Join the waitlist
       </a>
     </header>
--- src/components/WhyDetailedTable.jsx
+++ src/components/WhyDetailedTable.jsx
@@ -1,56 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import { featuresData } from "../utils/features-data";
 
 const WhyDetailedTable = () => {
-  const [hoveredColumn, setHoveredColumn] = useState(null);
 
   return (
     <div className="overflow-x-auto  w-full max-w-[400px] sm:max-w-[550px] md:max-w-full  p-10">
       <table className="table-auto w-full relative border-collapse text-left text-white">
         <thead>
-          <tr className="text-sm text-white border-b border-dashed border-neutral-700">
-            <th className="py-4 pr-9">Features</th>
+          <tr className="text-sm  border-b border-dashed border-neutral-700">
+            <th className="py-4 pr-9 text-white">Features</th>
             <th
-              className={`py-4 px-5 text-center relative ${
-                hoveredColumn === "NexBI"
-                  ? "bg-[#0B0D0ECC] text-white rounded-t-xl"
-                  : "text-[#FFFFFF26]"
-              }`}
-              onMouseEnter={() => setHoveredColumn("NexBI")}
-              onMouseLeave={() => setHoveredColumn(null)}
+              className={`py-4 px-5 text-center relative bg-[#0B0D0ECC] text-white rounded-t-xl`}
             >
               NexBI
             </th>
             <th
-              className={`py-4 px-8 text-center relative ${
-                hoveredColumn === "PowerBI"
-                  ? "bg-[#0B0D0ECC] text-white rounded-t-xl"
-                  : "text-[#FFFFFF26]"
-              }`}
-              onMouseEnter={() => setHoveredColumn("PowerBI")}
-              onMouseLeave={() => setHoveredColumn(null)}
+              className={`py-4 px-8 text-center relative text-[#FFFFFF26]`}
             >
               Power BI
             </th>
             <th
-              className={`py-4 px-8 text-center relative ${
-                hoveredColumn === "Qlik"
-                  ? "bg-[#0B0D0ECC] text-white rounded-t-xl"
-                  : "text-[#FFFFFF26]"
-              }`}
-              onMouseEnter={() => setHoveredColumn("Qlik")}
-              onMouseLeave={() => setHoveredColumn(null)}
+              className={`py-4 px-8 text-center relative text-[#FFFFFF26]`}
             >
               Qlik
             </th>
             <th
-              className={`py-4 px-8 text-center relative ${
-                hoveredColumn === "Supermetrics"
-                  ? "bg-[#0B0D0ECC] text-white rounded-t-xl"
-                  : "text-[#FFFFFF26]"
-              }`}
-              onMouseEnter={() => setHoveredColumn("Supermetrics")}
-              onMouseLeave={() => setHoveredColumn(null)}
+              className={`py-4 px-8 text-center relative text-[#FFFFFF26]`}
             >
               Supermetrics
             </th>
@@ -66,13 +41,7 @@ const WhyDetailedTable = () => {
             >
               <td className="py-5 pr-9">{feature.name}</td>
               <td
-                className={`py-5 px-6 text-center ${
-                  hoveredColumn === "NexBI"
-                    ? "bg-[#0B0D0ECC] cursor-pointer  text-white"
-                    : "text-[#FFFFFF26]"
-                }`}
-                onMouseEnter={() => setHoveredColumn("NexBI")}
-                onMouseLeave={() => setHoveredColumn(null)}
+                className={`py-5 px-6 text-center bg-[#0B0D0ECC] cursor-pointer  text-white`}
               >
                 {featuresData.length - 1 !== index ? (
                   feature.NexBI
@@ -83,35 +52,17 @@ const WhyDetailedTable = () => {
                 )}
               </td>
               <td
-                className={`py-5 px-8 text-center ${
-                  hoveredColumn === "PowerBI"
-                    ? "bg-[#0B0D0ECC] cursor-pointer text-white"
-                    : "text-[#FFFFFF26]"
-                }`}
-                onMouseEnter={() => setHoveredColumn("PowerBI")}
-                onMouseLeave={() => setHoveredColumn(null)}
+                className={`py-5 px-8 text-center  text-[#FFFFFF26]`}
               >
                 {feature.PowerBI}
               </td>
               <td
-                className={`py-5 px-8 text-center ${
-                  hoveredColumn === "Qlik"
-                    ? "bg-[#0B0D0ECC] cursor-pointer text-white"
-                    : "text-[#FFFFFF26]"
-                }`}
-                onMouseEnter={() => setHoveredColumn("Qlik")}
-                onMouseLeave={() => setHoveredColumn(null)}
+                className={`py-5 px-8 text-center  text-[#FFFFFF26]`}
               >
                 {feature.Qlik}
               </td>
               <td
-                className={`py-5 px-8 text-center ${
-                  hoveredColumn === "Supermetrics"
-                    ? "bg-[#0B0D0ECC] cursor-pointer text-white"
-                    : "text-[#FFFFFF26]"
-                }`}
-                onMouseEnter={() => setHoveredColumn("Supermetrics")}
-                onMouseLeave={() => setHoveredColumn(null)}
+                className={`py-5 px-8 text-center text-[#FFFFFF26]`}
               >
                 {feature.Supermetrics}
               </td>
